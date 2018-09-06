6137c4feb3aabf5ed761d33953868b09888aae3f
Jason Sutliff <jbopsut@gmail.com>
2018-09-05 23:00:56 -0700

    pseudo code added to customer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,19 @@
+//require dependencies
+
+
+//display all items from DB (include id, name and price)
+
+
+//prompt user for item id of product they would like to purchase
+  //prompt user how many they would like to buy
+
+  //check if stock > quantity response
+    //if stock !> quantity resp
+      // log insufficient quant
+
+    //else 
+      //fulfill order
+
+//func fulfillOrder
+  //update sql database
+  //show the total purchase cost to user
