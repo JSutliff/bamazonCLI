28e73a8e6d5816065ec52bdcb8cdcfe8c8a41354
Jason Sutliff <jbopsut@gmail.com>
2018-09-05 23:38:43 -0700

    dependencies added and display items func implemented

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,42 @@
 //require dependencies
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
+//establish connection to sqlDB
+var connection = mysql.createConnection({
+  host: 'localhost',
+
+  port: 3306,
+
+  user: 'root',
+
+  password: 'password',
+  database: 'bamazon'
+});
 
 //display all items from DB (include id, name and price)
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('connected ad id ' + connection.threadId);
+});
 
+function displayItems() {
+  connection.query('SELECT * FROM products', function(err, resp) {
+    if (err) throw err;
 
+    console.log(`
+    ================= Available Items =================`);
+    return resp.forEach(function(elem) {
+      console.log(`
+      Item ID: ${elem.item_id}
+      Product Name: ${elem.product_name}
+      Price: ${elem.price}
+      ----------------------------------------`);
+    })
+  })
+}
+
+displayItems();
 //prompt user for item id of product they would like to purchase
   //prompt user how many they would like to buy
 
